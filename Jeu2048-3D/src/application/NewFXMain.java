d677481ed8bafeccb09fe31ebc7567fc4b81384d
seak95 <ilan.mrt95@gmail.com>
2022-10-25 17:38:11 +0200

    Lié notre .fxml a .java

--- Jeu2048-3D/src/application/NewFXMain.java
+++ Jeu2048-3D/src/application/NewFXMain.java
@@ -5,11 +5,15 @@
  */
 package application;
 
+import java.io.IOException;
+import java.net.URL;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,25 +25,22 @@ public class NewFXMain extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+      // Localisation du fichier FXML.
+      final URL url = getClass().getResource("Jeu2048FXML.fxml");
+      // Création du loader.
+      final FXMLLoader fxmlLoader = new FXMLLoader(url);
+      // Chargement du FXML.
+      final AnchorPane root = (AnchorPane) fxmlLoader.load();
+      // Création de la scène.
+      final Scene scene = new Scene(root, 300, 250);
+      primaryStage.setScene(scene);
+    } catch (IOException ex) {
+      System.err.println("Erreur au chargement: " + ex);
     }
+    primaryStage.setTitle("C'est le 2048");
+    primaryStage.show();
+     }
 
     /**
      * @param args the command line arguments
